9b5a3be813c391d2b8b055fb04ed7ee83f339581
rootoor-dev <rootoor.projects@gmail.com>
2022-04-21 17:01:08 +0000

    polyglotor script in bash language.

---
+++ polyglotor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+: '
+----------------------------
+Author: ADI Junior "Rootoor"
+----------------------------
+
+SCRIPT: translate once many files in any language using command.
+For example, to translate the substitles of some movies in a desired language, this is the best way to do it. 
+Subtitles files get ".srt" as extension for their files.
+
+Requirements:  
+=============
+1) System Requirements to be found at "https://github.com/soimort/translate-shell"
+2) Install translate-shell (found at "https://github.com/soimort/translate-shell"  or "https://pkgs.org/download/translate-shell")
+
+RUN SCRIPT
+==========
+
+$ bash polyglotor.sh
+
+'
+for f in *.srt; do #==>> this line instructs the script to search for all the files in the current directory ending with .srt.
+	exec trans -b :fr -i "$f" -o "${f%.srt}.trans.srt"  #==>> line uses the trans command on each file found and having .srt extension to translate them
+done #==>> end of FOR LOOP
